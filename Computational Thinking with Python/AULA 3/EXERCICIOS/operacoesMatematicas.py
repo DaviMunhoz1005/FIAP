f11130fa34651faf14ade0a1c1580cb0a0b01ff1
DaviMunhoz1005 <davimunhoz1005@gmail.com>
2025-03-12 17:10:14 -0300

    feat: Create and solve exercises from AULA 3 in Computational Thinking with Python about order of operations

---
+++ Computational Thinking with Python/AULA 3/EXERCICIOS/operacoesMatematicas.py
@@ -0,0 +1,18 @@
+operation_1 = 10 + 20 * 30
+operation_2 = 4**2 / 3
+operation_3 = (94 + 2) * 6 - 1
+
+print(operation_1)
+print(operation_2)
+print(operation_3)
+
+#Objetivo da expressão abaixo é para comparar como a prioridade das expressões é importante
+operation_to_compare_in_real_life = 10 % 3 * 10 ** 2 + 1 - 10 * 4 / 2
+# 1 * 100 + 1 - 40 / 2 ==> 101 - 20 = 81
+print(operation_to_compare_in_real_life)
+
+# print(2 * 2 + 8 / 2 ** 2)
+# ==> 4 + 8 / 4 = 6
+
+# print(100 - 99 / 3 % 2)
+# ==> 100 - 33 % 2 ==> 100 - 1 = 99
