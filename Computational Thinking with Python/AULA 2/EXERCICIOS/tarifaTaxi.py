588070986bcc6456986ebe8bed6013e65a79cad2
DaviMunhoz1005 <davimunhoz1005@gmail.com>
2025-03-12 16:03:50 -0300

    feat: add the way to insert the minutes into the taxi rate exercise

--- Computational Thinking with Python/AULA 2/EXERCICIOS/tarifaTaxi.py
+++ Computational Thinking with Python/AULA 2/EXERCICIOS/tarifaTaxi.py
@@ -18,10 +18,11 @@ print("""
 
 miles_wheels = input('Insira o número de km rodados (exemplo - 20,5): ').replace(',', '.')
 hours_stop = input('Insira o número de horas esperadas parado (exemplo - 2,5): ').replace(',', '.')
+# minutes_stop = input('Insira o número de minutos esperados parado (exemplo - 150)').replace(',', '.')
 
 miles_wheels = float(miles_wheels)
-hours_stop = float(hours_stop)
+stopped_time = float(hours_stop) # float(minutes_stop) / 60
 
-fare = 5.5 + (2.8 * miles_wheels) + (hours_stop * 44)
+fare = 5.5 + (2.8 * miles_wheels) + (stopped_time * 44)
 
 print(f'A tarifa a ser paga será no valor de R${fare:.2f}')
