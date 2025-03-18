9cbc44c70cb6bac9281d56d9fb9e71d4d24b8aa8
DaviMunhoz1005 <davimunhoz1005@gmail.com>
2025-03-18 19:19:19 -0300

    feat: Update last exercise to include is not present variable

--- Computational Thinking with Python/AULA 4/EXERCICIOS/checkInputAno.py
+++ Computational Thinking with Python/AULA 4/EXERCICIOS/checkInputAno.py
@@ -5,6 +5,7 @@ actual_year = 2025
 is_past = year < actual_year
 is_present = year == actual_year
 is_future = year > actual_year
+is_not_present = is_past or is_future
 
 if is_past:
     print(f'O ano informado está no passado.')
