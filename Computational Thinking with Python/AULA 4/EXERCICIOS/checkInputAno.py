297704ef4bc9bff784910899bf45e97f9d50269b
DaviMunhoz1005 <davimunhoz1005@gmail.com>
2025-03-18 19:17:52 -0300

    feat: Create and solve exercise about year in past, present or future in Computational Thinking with Python

---
+++ Computational Thinking with Python/AULA 4/EXERCICIOS/checkInputAno.py
@@ -0,0 +1,14 @@
+year = input('Insira um ano: ')
+year = int(year)
+
+actual_year = 2025
+is_past = year < actual_year
+is_present = year == actual_year
+is_future = year > actual_year
+
+if is_past:
+    print(f'O ano informado está no passado.')
+elif is_present:
+    print(f'O ano informado é o presente.')
+else:
+    print(f'O ano informado está no futuro.')
