5124e31f2ae379ad6b7018c08af2a615f262b549
DaviMunhoz1005 <davimunhoz1005@gmail.com>
2025-03-18 20:27:06 -0300

    feat: Update last exercise to include leap year in the scope

--- Computational Thinking with Python/AULA 5/EXERCICIOS/checkYearCopaOlimpiada.py
+++ Computational Thinking with Python/AULA 5/EXERCICIOS/checkYearCopaOlimpiada.py
@@ -4,6 +4,12 @@ year = int(year)
 
 is_olympics = (year - 2020) % 4 == 0
 is_cup = year % 4 == 2
+is_leap_year = year % 4 == 0
+
+if year % 100 == 0:
+    is_leap_year = year % 400 == 0
+
+print(f'O ano {year} {"será" if is_leap_year else "não será"} ano bissexto')
 
 if is_olympics:
     print(f'No ano de {year} terá olimpíadas.')
