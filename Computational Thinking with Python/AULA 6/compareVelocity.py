858aa425dadfc0bc511104639c2484358fe6f7a9
DaviMunhoz1005 <davimunhoz1005@gmail.com>
2025-03-25 20:37:14 -0300

    feat: update exercise about velocity car to include fine value in AULA 6 from Computational Thinking with Python

--- Computational Thinking with Python/AULA 6/compareVelocity.py
+++ Computational Thinking with Python/AULA 6/compareVelocity.py
@@ -1,10 +1,17 @@
-velocity_car = input('Qual a velocidade do carro: ')
-velocity_car = float(velocity_car)
+velocity_car = float(input('Qual a velocidade do carro: '))
+max_velocity = float(input('Qual a velocidade máxima da rua: '))
 
-max_velocity = input('Qual a velocidade máxima da rua: ')
-max_velocity = float(max_velocity)
-
-if velocity_car > max_velocity:
-    print(f'Tomou Multa')
-else:
+if velocity_car <= max_velocity:
     print(f'Não Tomou Multa')
+else:
+    percentage_ahead = velocity_car / max_velocity
+
+    fine_value = 0
+    if percentage_ahead >= 1.5:
+        fine_value = 880.41
+    elif percentage_ahead > 1.2:
+        fine_value = 195.23
+    else:
+        fine_value = 130.16
+
+    print(f'Tomou Multa no valor de {fine_value}')
