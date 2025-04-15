0690a66f2f2c709305a0383893c457f78c7b60be
DaviMunhoz1005 <davimunhoz1005@gmail.com>
2025-04-14 22:37:44 -0300

    feat(WEb Development): Start using if else and switch

---
+++ Web Development/AULA 7/App.js
@@ -0,0 +1,8 @@
+let username = prompt("User: ");
+let password = prompt("Password: ");
+
+if (password === "1234" && username === "1234") {
+    alert("Logado com Sucesso!");
+} else {
+    alert("Username ou senha inválidos!");
+}
