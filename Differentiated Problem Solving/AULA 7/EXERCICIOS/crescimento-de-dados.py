5144920112b73a067e3f5bbea050128d9d73c37b
DaviMunhoz1005 <davimunhoz1005@gmail.com>
2025-04-28 15:26:37 -0300

    feat(Differentiated Problem Solving): Create EXERCISE in AULA 7 and solve exercise about increment in Database

--- Differentiated Problem Solving/AULA 7/EXERCICIOS/crescimento-de-dados.py
+++ Differentiated Problem Solving/AULA 7/EXERCICIOS/crescimento-de-dados.py
@@ -1,19 +1,18 @@
-import numpy
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.linspace(1, 8, 48)
-a, b = [100, 2]
+x = np.linspace(1, 20, 60)
+a, b = (1000, 1.3)
 
 
 def f(x):
     return a * b ** x
 
 
-plt.title("Exemplo Função Exponencial")
+plt.title("Exercício 1: Crescimento de dados em Database")
 plt.plot(x, f(x), label=f"y = {a} * {b} ** x", color="blue")
-plt.scatter(6, f(6), label=f"Número de Users após 6 meses = {f(6)}", color="red")
-plt.text(6, f(6), f"({6}, {f(6)})", color="red")
+plt.scatter(15, f(15), label=f"Número de Registros após 15 dias = {f(15):.0f}", color="red")
+plt.text(15, f(15), f"({15}, {f(15):.0f})", color="red")
 plt.axhline(color="black", linewidth="1")
 plt.axvline(color="black", linewidth="1")
 plt.legend()
