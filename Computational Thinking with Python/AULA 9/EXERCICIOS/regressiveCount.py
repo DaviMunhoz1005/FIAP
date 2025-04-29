5d9594a6c119b94cba2a64c85722df9317b3d461
DaviMunhoz1005 <davimunhoz1005@gmail.com>
2025-04-29 20:27:52 -0300

    feat(Computational Thinking with Python): create and solve exercise in AULA 9 to make a regressive count

---
+++ Computational Thinking with Python/AULA 9/EXERCICIOS/regressiveCount.py
@@ -0,0 +1,9 @@
+def regressive_count():
+    start_number = 10
+    while start_number >= 0:
+        print(start_number)
+        start_number = start_number - 1
+    print("Fogo!")
+
+
+regressive_count()
