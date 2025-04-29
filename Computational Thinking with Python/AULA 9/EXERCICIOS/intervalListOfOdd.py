ba8231c857b9a2b0833289601fd0aea64e96b058
DaviMunhoz1005 <davimunhoz1005@gmail.com>
2025-04-29 20:30:24 -0300

    feat(Computational Thinking with Python): create and solve exercise in AULA 9 to take a interval off odd numbers

---
+++ Computational Thinking with Python/AULA 9/EXERCICIOS/intervalListOfOdd.py
@@ -0,0 +1,11 @@
+def get_interval_numbers(end):
+    numbers = []
+    start_number = 1
+    while start_number <= end:
+        if start_number % 2 == 0:
+            numbers.append(start_number)
+        start_number = start_number + 1
+    return numbers
+
+
+print(get_interval_numbers(10))
