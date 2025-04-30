61abbb4222fb220b3e62415420e5ee9015cbff01
DaviMunhoz1005 <davimunhoz1005@gmail.com>
2025-04-29 21:25:23 -0300

    feat(Computational Thinking with Python): create and solve exercise in AULA 9 to check if a number is Prime

---
+++ Computational Thinking with Python/AULA 9/EXERCICIOS/isPrime.py
@@ -0,0 +1,15 @@
+def isPrime(number):
+    start = 2
+    while start <= number / 2:
+        if number % start == 0:
+            return False
+        start = start + 1
+    return True
+
+
+print(isPrime(2))
+print(isPrime(3))
+print(isPrime(53))
+print(isPrime(5))
+print(isPrime(97))
+
