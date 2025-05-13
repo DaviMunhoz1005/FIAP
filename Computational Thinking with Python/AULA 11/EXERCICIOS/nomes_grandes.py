1550ce816921ada0d9268af257e12771f571e125
DaviMunhoz1005 <davimunhoz1005@gmail.com>
2025-05-13 19:41:04 -0300

    feat(Computational Thinking with Python): create and solve exercise about names greater than 10

---
+++ Computational Thinking with Python/AULA 11/EXERCICIOS/nomes_grandes.py
@@ -0,0 +1,15 @@
+def bigger_name(names: list[str]) -> list[str]:
+    bigger_names = []
+    for name in names:
+        if len(name) > 10:
+            bigger_names.append(name)
+
+    return bigger_names
+
+
+assert bigger_name(["Joao", "Constantinos"]) == ["Constantinos"]
+assert bigger_name([]) == [],
+assert bigger_name(["Ana", "Pedro", "Alexandrino"]) == ["Alexandrino"]
+assert bigger_name(["Alexandrino", "Bernardinos", "Carlos"]) == ["Alexandrino", "Bernardinos"]
+assert bigger_name(["Maria", "Jose", "Gabrielinos"]) == ["Gabrielinos"]
+print("Todos os testes passam!")
